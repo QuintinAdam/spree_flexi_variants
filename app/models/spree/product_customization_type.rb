7e7ded3f0ff9e25326faf3200164ee01fedeb49f
Hugo Santos <hugo.santos@factorlibre.com>
2014-12-15 19:04:08 +0100

    Fixed include Spree::CalculatedAdjustments

--- app/models/spree/product_customization_type.rb
+++ app/models/spree/product_customization_type.rb
@@ -1,6 +1,6 @@
 module Spree
   class ProductCustomizationType < ActiveRecord::Base
-    include Spree::Core::CalculatedAdjustments
+    include Spree::CalculatedAdjustments
     has_and_belongs_to_many :products
     has_many :customizable_product_options, :dependent => :destroy
     accepts_nested_attributes_for :customizable_product_options, :allow_destroy => true
