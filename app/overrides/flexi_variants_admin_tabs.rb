085dc06fad1613a5c89ea7fa00f1d2560b8b9fcc
Quintin Adam <quintinadam@live.com>
2015-03-23 16:44:13 -0600

    deface change

--- app/overrides/flexi_variants_admin_tabs.rb
+++ app/overrides/flexi_variants_admin_tabs.rb
@@ -1,8 +1,8 @@
 # admin_tabs data-hook missing changing to look at main_menu bar
 Deface::Override.new(
-  virtual_path: "/spree/admin/shared/_main_menu",
+  virtual_path: "spree/admin/shared/_main_menu",
   name: "flexi_variants_admin_tabs",
-  insert_after: "ul") do
+  insert_after: ".nav-sidebar:last-child") do
   '<% if can? :admin, Spree::Product %>
     <ul class="nav nav-sidebar">
       <%= tab :product_customization_types, url: spree.admin_product_customization_types_path, icon: "option-horizontal" %>
