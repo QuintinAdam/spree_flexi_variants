f7d10bf64f38f04a18e8a42977ce57b35ca85ef3
Quintin Adam <quintinadam@live.com>
2015-03-24 11:08:06 -0600

    removed product_sub_menu moved sub_menu customization_types link to products tab

--- app/overrides/add_admin_tab_for_product_customization_types.rb
+++ app/overrides/add_admin_tab_for_product_customization_types.rb
@@ -1,7 +1,8 @@
 # no such file spree/admin/shared/_product_sub_menu
-Deface::Override.new(
-  virtual_path: "spree/admin/shared/_product_sub_menu",
-  name: "converted_admin_product_sub_tabs_203014347",
-  insert_bottom: "[data-hook='admin_product_sub_tabs'], #admin_product_sub_tabs[data-hook]",
-  text: %{<%= tab :product_customization_types, match_path: '/product_customization_types'}
-)
+# this shouldn't be needed now
+# Deface::Override.new(
+#   virtual_path: "spree/admin/shared/_product_sub_menu",
+#   name: "converted_admin_product_sub_tabs_203014347",
+#   insert_bottom: "[data-hook='admin_product_sub_tabs'], #admin_product_sub_tabs[data-hook]",
+#   text: %{<%= tab :product_customization_types, match_path: '/product_customization_types'}
+# )
