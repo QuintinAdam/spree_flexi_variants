0f6abb5e110f5e4fa0cf851dfba4a1aab2b5bbb0
Quintin Adam <quintinadam@live.com>
2015-05-22 10:10:53 -0600

    customizations and options can be seen on the admin order cart view

--- app/overrides/flexi_variants_admin_tabs.rb
+++ app/overrides/flexi_variants_admin_tabs.rb
@@ -3,6 +3,6 @@ Deface::Override.new(
   virtual_path: "spree/admin/shared/sub_menu/_product",
   name: "flexi_variants_admin_tabs",
   insert_bottom: "#sidebar-product",
+  original: '1950c492f6ee166b7d4eedb6b9076ab3e986f110',
   text: '<%= tab :customization_types, url: spree.admin_product_customization_types_path %>'
 )
-
