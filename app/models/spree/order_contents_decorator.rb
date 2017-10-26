a602baeebf3772b80cb730a8e151cf33aace0737
Gabriel Custodio <gcmartins93@gmail.com>
2017-10-26 19:08:57 -0200

    fixed unable to add to cart - issue added by spree 3.3

--- app/models/spree/order_contents_decorator.rb
+++ app/models/spree/order_contents_decorator.rb
@@ -10,7 +10,11 @@ module Spree
         line_item.quantity += quantity.to_i
         line_item.currency = currency unless currency.nil?
       else
-        opts = { currency: order.currency }.merge ActionController::Parameters.new(options).permit(PermittedAttributes.line_item_attributes)
+        options_params = options.is_a?(ActionController::Parameters) ? options : ActionController::Parameters.new(options.to_h)
+        opts = options_params.
+          permit(PermittedAttributes.line_item_attributes).to_h.
+          merge(currency: order.currency)
+
         line_item = order.line_items.new(quantity: quantity, variant: variant, options: opts)
 
         product_customizations_values = options[:product_customizations] || []
